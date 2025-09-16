b64bb1f7be34a5858075cffa15ea6f08cde861a7
Mahi Bhatia <mahibhatia2004@gmail.com>
2025-09-16 07:25:29 +0530

    Create app.jsx

---
+++ Frontend/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Home from "./pages/Home.jsx";
+
+export default function App() {
+  return (
+    <div>
+      <h1>MedPredict</h1>
+      <Home />
+    </div>
+  );
+}
