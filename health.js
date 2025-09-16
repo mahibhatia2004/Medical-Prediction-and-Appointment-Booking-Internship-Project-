070321626859555defb88e84b127c3f0132a3df3
Mahi Bhatia <mahibhatia2004@gmail.com>
2025-09-16 07:22:23 +0530

    Create health.js

---
+++ health.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.json({ status: "ok", message: "Backend is running" });
+});
+
+export default router;
